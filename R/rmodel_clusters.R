98f95c13d7607d706ad419aecd982e5fafe89095
rocalabern <rocalabern@gmail.com>
2015-05-14 00:56:46 +0200

    cluster tools

---
+++ R/rmodel_clusters.R
@@ -0,0 +1,35 @@
+#' @export
+r.clusters.rearrage <- function (real, model) {
+  values = unique(real)
+  n = length(values)
+  for (i in 2:n) {
+    for (j in 1:(i-1)) {
+      scoreAct = sum(as.integer(real==values[i])*as.integer(model==values[i]))
+      scoreNew = sum(as.integer(real==values[i])*as.integer(model==values[j]))
+      if (scoreNew > scoreAct) {
+        indi = model==values[i]
+        indj = model==values[j]
+        model[indi] = values[j]
+        model[indj] = values[i]
+      }
+    }
+  }
+}
+
+#' @export
+r.clusters.kmeans <- function (data, k) {
+  result = kmeans(data, k)
+  return (list(result=result, centers=result$centers, clusters=result$cluster))
+}
+
+#' @export
+r.clusters.pam <- function (data, k) {
+  result = cluster::pam(data, k)
+  return (list(result=result, centers=result$medoids, clusters=result$clustering))
+}
+
+#' @export
+r.clusters.silhouette <- function (data, clusters, dist = dist(data, "euclidean")) {
+  result = cluster::silhouette(data, dist)
+  return (result)
+}
